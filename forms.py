6f60d669627ae6ec8cbc0aac1a27986a6db91e08
cbudoya <cbudoya@gmail.com>
2012-11-13 18:11:31 +0300

    adding t commit

---
+++ forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from doctor.models import country
+# Create your forms here.
+    
+class ContactForm(forms.Form):
+    first_name = forms.CharField(max_length=40)
+    middle_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=40)
+    gender = forms.CharField(max_length=10)
+    date_of_birth = forms.DateField( )
+    nationality = forms.ModelChoiceField(country.objects.all())
+    postal_address = forms.CharField(max_length=100)
+    vodacom_phone = forms.IntegerField()
+    other_phone = forms.IntegerField()
+    email_address = forms.EmailField()

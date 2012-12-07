4f0aaf39acfa54a824b5d50e69528fd4b9477826
cbudoya <cbudoya@gmail.com>
2012-12-07 17:36:23 +0300

    profession models added

---
+++ switchboard_api/profession/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from profession_category.models import Profession_category
+from contact.models import Contact
+
+
+class Profession(models.Model):
+    # contact is a foreign key from contact of a doctor
+    contact = models.ForeignKey(Contact, related_name='Profession')
+    # profession category is a foreign key from profession category
+    profession_category = models.ForeignKey(Profession_category,
+                                          related_name='Profession')
+    # specialization of a doctor in that profession category 
+    specialization = models.CharField(max_length=50)
+    # date of data entry
+    date = models.DateField()
+    
+    def __unicode__(self):
+        return u'%s' % (self.profession_name)
+

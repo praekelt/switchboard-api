2faf7288cd014b369585986abae569f9d8114da5
cbudoya <cbudoya@gmail.com>
2012-12-10 12:12:35 +0300

    application model edited

--- switchboard_api/application/admin.py
+++ switchboard_api/application/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from switchboard_api.application.models import Application
 
 admin.site.register(Application)
--- switchboard_api/application/admin.py~
+++ switchboard_api/application/admin.py~
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from switchboard_api.application.models import Application
 
 admin.site.register(Application)
